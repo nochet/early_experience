d01420be33580242c7adf7fb618cd10fd1732958
nochet <ngomae@missouri.edu>
2020-11-29 23:50:55 -0600

    Phenotype figures for fecundity

--- scripts/color_map_blindr.R
+++ scripts/color_map_blindr.R
@@ -1,13 +1,14 @@
 # Functions for color mapping
 
-pal <- c("#999999", "#E69F00", "#56B4E9")
+pal <- c("#999999", "#E69F00")
+#pal <- c("#999999", "#E69F00", "#56B4E9")
 
 tx_fill_map <- function(){
   cmap <- scale_fill_OkabeIto(
     name = "Diet",
     values = c("C" = pal[1],
-               "DR" = pal[2],
-               "HS" = pal[3]))
+               "DR" = pal[2]))
+               #"HS" = pal[3]))
   return(cmap)
 }
 
@@ -15,7 +16,7 @@ tx_color_map <- function(){
   cmap <- scale_color_OkabeIto(
     name = "Diet",
     values = c("C" = pal[1],
-               "DR" = pal[2],
-               "HS" = pal[3]))
+               "DR" = pal[2]))
+               #"HS" = pal[3]))
   return(cmap)
 }
